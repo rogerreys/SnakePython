aa06d5857aba045e956f99ee7e664111c1fa609b
Roger Reyes <rreyes@cobiscorp.com>
2024-01-03 21:13:36 -0500

    Ajuste app y clases nuevas

--- app.py
+++ app.py
@@ -1,48 +1,61 @@
 import pygame, sys
 import random
-from src.Snake import Snake;
-from src.Food import Food;
+from src.snake import Snake;
+from src.snake_actions import SnakeActions;
+from src.food import Food;
 
-pygame.init()
-# Screen
-size = width, height = 1366, 768
-screen = pygame.display.set_mode(size)
 
-# 
-snake = Snake()
-food = Food(200, 200)
+class SnakeGame:
+    def __init__(self):
+        # Screen
+        self.width = 1366
+        self.height = 768
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.clock = pygame.time.Clock()
+        # 
+        self.snake = Snake(0, 0, 10, 10)
+        self.snake_segments = [self.snake]
+        self.snake_action = SnakeActions(self.snake_segments[0])
+        self.food = Food(200, 200)
 
-#
-all_sprites = pygame.sprite.Group()
-all_sprites.add(snake)
-all_sprites.add(food)
-
-while True:
-    # Procesar los eventos
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+        #
+        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites.add(self.snake_segments)
+        self.all_sprites.add(self.food)
     
-    # Mover la serpiente
-    snake.update(screen)
-    snake.move()
+    def run(self):
+        while True:
+            # Procesar los eventos
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            # Mover la serpiente
+            self.snake_action.update(self.snake_segments, self.screen)
 
-    # Verificar colisiones
-    if snake.rect.colliderect(food.rect):
-        # Crear una nueva pieza de comida
-        all_sprites.remove(food)
-        food = Food(random.randint(0, width - 10), random.randint(0, height - 10))
-        all_sprites.add(food)
-        # La serpiente ha comido la comida
-        # Aumentar el tamaño de la serpiente
-        snake.length += 10
-        snake.grow()
-        
+            self.snake_action.move()
 
-    # Dibujar los objetos en la pantalla
-    screen.fill((0, 0, 0))
-    all_sprites.draw(screen)
-       
-    # Actualizar la pantalla
-    pygame.display.flip()
+            # Verificar colisiones
+            if self.snake.rect.colliderect(self.food.rect):
+                # Crear una nueva pieza de comida
+                self.all_sprites.remove(self.food)
+                self.food = Food(random.randint(0, self.width - 10), random.randint(0, self.height - 10))
+                self.all_sprites.add(self.food)
+                # La serpiente ha comido la comida
+                # Aumentar el tamaño de la serpiente
+                self.snake.length += 10
+                self.snake.grow()
+                
+
+            # Dibujar los objetos en la pantalla
+            self.screen.fill((0, 0, 0))
+            self.all_sprites.draw(self.screen)
+            
+            # Actualizar la pantalla
+            pygame.display.flip()
+
+
+if __name__ == "__main__":
+    game = SnakeGame()
+    game.run()
--- src/Snake.py
+++ src/Snake.py
@@ -1,43 +1,18 @@
 import pygame, sys
 
 class Snake(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self,  x, y, width, height):
         super().__init__()
-        self.image = pygame.Surface((10, 10))
+        self.image = pygame.Surface((width, height))
         self.image.fill((0, 255, 0))
         self.rect = self.image.get_rect()
-        self.rect.x = 0
-        self.rect.y = 0
+        self.rect.x = x
+        self.rect.y = y
 
         self.direction = pygame.Vector2(0, 1)
         self.grow_size = pygame.Vector2(10, 10)
         self.length = 10
 
-    def update(self, screen):
-        self.rect.x += self.direction.x
-        self.rect.y += self.direction.y
-        
-        # self.image.set_height(self.grow_size.y) 
-        # self.image.set_width(self.grow_size.x)
-        # self.image = nueva_superficie;
-        if self.rect.x < 0 or self.rect.x > screen.get_width() or self.rect.y < 0 or self.rect.y > screen.get_height():
-            pygame.quit()
-            sys.exit()
-
-    def move(self):
-        # Control player2
-        if (pygame.key.get_pressed()[pygame.K_UP]): 
-            self.direction = pygame.Vector2(0, -1)
-            self.position_y()
-        if (pygame.key.get_pressed()[pygame.K_DOWN]): 
-            self.direction = pygame.Vector2(0, 1)
-            self.position_y()
-        if (pygame.key.get_pressed()[pygame.K_RIGHT]): 
-            self.direction = pygame.Vector2(1, 0)
-            self.position_x()
-        if (pygame.key.get_pressed()[pygame.K_LEFT]): 
-            self.direction = pygame.Vector2(-1, 0)
-            self.position_x()
 
     def grow(self):
         if (pygame.key.get_pressed()[pygame.K_UP] or pygame.key.get_pressed()[pygame.K_DOWN]):
